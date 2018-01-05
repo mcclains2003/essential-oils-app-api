68d6c5ca634d5455446fa994f605303d8f96ac60
mcclains2003 <mcclains2003@gmail.com>
2018-01-05 09:23:28 -0600

    switched from sqlite to postgres in Gemfile, changed the datbase.yml file to reflect postgres switch, schema changed because of this.

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171128175241) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "oils", force: :cascade do |t|
     t.string "name"
     t.text "description"
