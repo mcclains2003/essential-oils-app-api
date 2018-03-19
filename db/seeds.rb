4add13ea635da32e5498fc5a72772b272f226c43
mcclains2003 <mcclains2003@gmail.com>
2018-03-19 09:41:50 -0500

    Created seed data for posting on heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# create_table "oils", force: :cascade do |t|
+#     t.string "name"
+#     t.text "description"
+#     t.text "uses"
+#     t.text "fragrance_profile"
+#     t.text "medical_properties"
+#     t.datetime "created_at", null: false
+#     t.datetime "updated_at", null: false
+#   end
+
+# vc1 = VolumeCost.create([ volume: 2, cost: 4, category: category1 ])
+
+Oil.create([name: "Test Name", description: "Test Description", uses: "Test Uses", fragrance_profile: "Test Fragrance", medical_properties:"Test Medical Properties"])
