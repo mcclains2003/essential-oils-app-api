a9aa0071ad7bc340949987f1a8ca94f76d8e72c9
mcclains2003 <mcclains2003@gmail.com>
2018-03-20 12:04:58 -0500

    updated the cors file in initializers

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['localhost:3000', 'localhost:3001']
+    origins ['localhost:3000', 'localhost:3001', 'https://eo-directory.herokuapp.com']
 
     resource '*',
       headers: :any,
